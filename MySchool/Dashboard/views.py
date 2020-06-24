d10e1b4786a0c65d32d096a4a2f18290d9bc2558
Aagam41 <aagam.h.sheth@icloud.com>
2020-06-24 21:07:48 +0530

    I have updated the directory for django. And urge you to also give a try cloning the git repo and editing it an uploading it. Also try branching. it is what git stands for.

---
+++ MySchool/Dashboard/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def Courses(request):
+    return HttpResponse('<h1>Ths is my Home Page</h1>')
